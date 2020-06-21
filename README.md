7c31f3f70c560f7c6242134b88f16316f1a0a5bc
ogapan <ogapamyu@gmail.com>
2020-06-21 23:57:06 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Add hl=en or hl=ja to any URL (AHL)
+
+![icon](icon.png)
+
---
+++ script.js
@@ -0,0 +1,19 @@
+alert('Hello, world!');
+
+//hmm..
+// console.log("JJJJJ")
+
+// function hlen() {
+//   console.log("click en")
+//   if (document.URL.match(/\?/)) {
+//     location.href = document.URL + "&hl=en"
+//   } else {
+//     location.href = document.URL + "?hl=en"
+//   }
+//   return;
+// }
+
+// document.getElementById('hlen').addEventListener('click', hlen);
+// document.getElementById('hlja').addEventListener('click', hlen);//FIXME
+
+// console.log("SSSSSS")
